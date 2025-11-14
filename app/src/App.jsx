09e9f4185cac1b43a78ee81c7dfec973e9f12f6a
DiegoNoguez <noguezlopezarturo@gmail.com>
2025-11-14 13:37:59 -0600

    Corrección en la gama de colores y errores tipográficos.

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -27,18 +27,19 @@ function App() {
   }
 
   return (
-    <>
-      <div className="min-h-screen-bg-[#1e1e1e] text-gray-200 p-6 font-mono">
-        <div className = "max-w-4xl mx-auto space-y-4">
+      <>
+      <div className="min-h-screen bg-[#1e1e1e] text-gray-200 p-6 font-mono">
+        <div className="max-w-4xl mx-auto space-y-4">
           <h1 className="text-xl font-semibold text-[#d4d4d4]">MiniLang</h1>
 
           {/* Textarea de entrada*/}
-          <textarea  id = "text"
+          <textarea  
+            id="text"
             onChange={handleChange}
             className="w-full h-48 bg-[#1e1e1e] text-[#d4d4d4] border border-[#3c3c3c]
                        rounded-md p-3 focus:outline-none focus:border-[#007acc]
                        placeholder-gray-500 resize-none shadow-inner"
-            placeholder = "Esribe algo aqui...">
+            placeholder="Escribe algo aqui...">
           </textarea>
 
           {/* TEXTAREA SALIDA*/ }
@@ -56,13 +57,14 @@ function App() {
             className={`px-4 py-2 rounded-md font-semibold transition
               ${loading
                 ? 'bg-[#0e639c] opacity-60 cursor-not-allowed'
-                : 'bg-[#0e639c] hover:bg-[#1177bbb]'}
+                : 'bg-[#0e639c] hover:bg-[#1177bb]'}
             `}>
               {loading ? 'Enviando...':'Enviar'}
             </button>
         </div>
       </div>
     </>
+
   )
 }
 
